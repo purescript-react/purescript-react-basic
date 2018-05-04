1de8f03cba42f79fb8685124dbe12bd68ddabe75
Michael Trotter <spicydonuts@users.noreply.github.com>
2018-05-04 16:47:31 -0600

    Update examples (#33)

--- examples/component/src/Container.purs
+++ examples/component/src/Container.purs
@@ -5,11 +5,12 @@ import React.Basic.DOM as R
 import ToggleButton as ToggleButton
 
 component :: ReactComponent {}
-component = stateless
-  { displayName: "Container"
-  , render: \_ ->
-      R.div { children: [ createElement ToggleButton.component { on: true }
-                        , createElement ToggleButton.component { on: false }
-                        ]
-            }
-  }
+component = stateless { displayName: "Container", render }
+  where
+    render _ =
+      R.div
+        { children:
+            [ createElement ToggleButton.component { on: true }
+            , createElement ToggleButton.component { on: false }
+            ]
+        }
--- examples/controlled-input/src/ControlledInput.purs
+++ examples/controlled-input/src/ControlledInput.purs
@@ -3,27 +3,34 @@ module ControlledInput where
 import Prelude
 
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
-import React.Basic (ReactComponent, react)
+import React.Basic (ReactComponent, fragment, react)
 import React.Basic.DOM as R
 import React.Basic.DOM.Events (preventDefault, targetValue, timeStamp)
 import React.Basic.Events as Events
 
 component :: ReactComponent {}
-component = react
-  { displayName: "ControlledInput"
-  , initialState: { value: "hello world", timeStamp: Nothing }
-  , receiveProps: \_ _ _ -> pure unit
-  , render: \_ state setState ->
-      R.div_
-        [ R.p_ [ R.input { onChange: Events.handler (preventDefault >>> Events.merge { targetValue, timeStamp })
-                             \{ timeStamp, targetValue } -> setState \_ ->
-                                { value: fromMaybe "" targetValue
-                                , timeStamp: Just timeStamp
-                                }
-                         , value: state.value
-                         }
-               ]
+component = react { displayName: "ControlledInput", initialState, receiveProps, render }
+  where
+    initialState =
+      { value: "hello world"
+      , timeStamp: Nothing
+      }
+
+    receiveProps _ _ _ =
+      pure unit
+      
+    render _ state setState =
+      fragment
+        [ R.input
+            { onChange:
+                Events.handler
+                  (preventDefault >>> Events.merge { targetValue, timeStamp })
+                  \{ timeStamp, targetValue } ->
+                    setState _ { value = fromMaybe "" targetValue
+                               , timeStamp = Just timeStamp
+                               }
+            , value: state.value
+            }
         , R.p_ [ R.text ("Current value = " <> show state.value) ]
         , R.p_ [ R.text ("Changed at = " <> maybe "never" show state.timeStamp) ]
         ]
-  }
