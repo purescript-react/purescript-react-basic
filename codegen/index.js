637eb8b5ef2b65a169097962f1f85412f9f4257a
Justin Woo <moomoowoo@gmail.com>
2018-07-31 19:05:17 +0300

    Export consts in codegen (#50)

--- codegen/index.js
+++ codegen/index.js
@@ -1,48 +1,6 @@
-const props = require("react-html-attributes");
-const voids = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"];
-const types = {
-  "allowFullScreen": "Boolean",
-  "async": "Boolean",
-  "autoPlay": "Boolean",
-  "capture": "Boolean",
-  "checked": "Boolean",
-  "children": "Array JSX",
-  "cols": "Number",
-  "controls": "Boolean",
-  "default": "Boolean",
-  "defer": "Boolean",
-  "disabled": "Boolean",
-  "formNoValidate": "Boolean",
-  "hidden": "Boolean",
-  "itemScope": "Boolean",
-  "loop": "Boolean",
-  "max": "Number",
-  "min": "Number",
-  "multiple": "Boolean",
-  "muted": "Boolean",
-  "onClick": "EventHandler",
-  "onInput": "EventHandler",
-  "onInvalid": "EventHandler",
-  "onSubmit": "EventHandler",
-  "noValidate": "Boolean",
-  "onChange": "EventHandler",
-  "open": "Boolean",
-  "playsInline": "Boolean",
-  "readOnly": "Boolean",
-  "required": "Boolean",
-  "reversed": "Boolean",
-  "rowSpan": "Number",
-  "rows": "Number",
-  "scoped": "Boolean",
-  "seamless": "Boolean",
-  "selected": "Boolean",
-  "size": "Number",
-  "span": "Number",
-  "start": "Number"
-};
-const reserved = ["module", "data", "type", "newtype", "class", "instance", "where", "derive", "if", "then", "else", "case", "of"];
+const { props, voids, types, reserved } = require('./consts');
 
-printRecord = (elProps) => elProps.length ? `
+const printRecord = (elProps) => elProps.length ? `
   ( ${ elProps.map((p) =>
        `${p} :: ${types[p] || "String"}`).join("\n  , ")
      }
