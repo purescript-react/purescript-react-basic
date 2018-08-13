c3266155d9b8284e7e520231ac0afdf4400ef43e
Michael Trotter <spicydonuts@gmail.com>
2018-08-13 15:59:44 -0600

    Add DOM functions: render, hydrate, unmount

--- src/React/Basic/DOM.js
+++ src/React/Basic/DOM.js
@@ -2,6 +2,18 @@
 
 var ReactDOM = require("react-dom");
 
+exports.render_ = function(jsx, node, callback) {
+  ReactDOM.render(jsx, node, callback);
+};
+
+exports.hydrate_ = function(jsx, node, callback) {
+  ReactDOM.hydrate(jsx, node, callback);
+};
+
+exports.unmountComponentAtNode_ = function(node) {
+  return ReactDOM.unmountComponentAtNode(node);
+};
+
 exports.findDOMNode_ = function(instance) {
   return ReactDOM.findDOMNode(instance);
 };
