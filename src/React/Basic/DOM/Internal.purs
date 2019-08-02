0b45890df3863b6e8e44e3c360ef0565fe2fea9e
Madeline Trotter <spicydonuts@gmail.com>
2019-08-02 09:28:40 -0700

    Add shared xml namespace props

--- src/React/Basic/DOM/Internal.purs
+++ src/React/Basic/DOM/Internal.purs
@@ -11,23 +11,36 @@ foreign import data CSS :: Type
 -- The string props are from MDN, and the
 -- event handlers are the same as in SharedProps
 -- (the same events should work for SVG elements)
-type SharedSVGProps specific =
-  ( id :: String
-  , className :: String
-  , style :: CSS
-  , tabIndex :: String
-  , onBlur            :: EventHandler
-  , onClick           :: EventHandler
-  , onFocus           :: EventHandler
-  , onMouseDown       :: EventHandler
-  , onMouseEnter      :: EventHandler
-  , onMouseLeave      :: EventHandler
-  , onMouseMove       :: EventHandler
-  , onMouseOut        :: EventHandler
-  , onMouseOver       :: EventHandler
-  , onMouseUp         :: EventHandler
-  | specific
-  )
+type SharedSVGProps specific
+  = ( id :: String
+    , className :: String
+    , style :: CSS
+    , tabIndex :: String
+    , lang :: String
+    , xlinkActuate :: String
+    , xlinkArcrole :: String
+    , xlinkHref :: String
+    , xlinkRole :: String
+    , xlinkShow :: String
+    , xlinkTitle :: String
+    , xlinkType :: String
+    , xmlBase :: String
+    , xmlLang :: String
+    , xmlSpace :: String
+    , xmlns :: String
+    , xmlnsXlink :: String
+    , onBlur :: EventHandler
+    , onClick :: EventHandler
+    , onFocus :: EventHandler
+    , onMouseDown :: EventHandler
+    , onMouseEnter :: EventHandler
+    , onMouseLeave :: EventHandler
+    , onMouseMove :: EventHandler
+    , onMouseOut :: EventHandler
+    , onMouseOver :: EventHandler
+    , onMouseUp :: EventHandler
+    | specific
+    )
 
 unsafeCreateDOMComponent :: forall props. String -> ReactComponent props
 unsafeCreateDOMComponent = unsafeCoerce
